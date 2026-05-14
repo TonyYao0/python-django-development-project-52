611d09ff45f03632dd090b48e89873b8b0a9d048
TonyYao0 <tony_yao@mail.ru>
2026-05-14 16:28:59 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-make install && python manage.py compilemessages && make collectstatic && make migrate
+
+make install
+
+uv run python manage.py compilemessages
+
+
+make collectstatic
+make migrate
